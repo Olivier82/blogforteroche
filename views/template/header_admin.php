a51246b095643dbd67e6f0d1504cc582d7373255
Bosco91 <olivier@chanteloube.me>
2019-07-17 14:40:37 +0200

    Ajout header administration

---
+++ views/template/header_admin.php
@@ -0,0 +1,38 @@
+<doctype HTML>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="Blog de Jean Forteroche, pour son roman un billet simple pour l'alaska">
+        <meta name="author" content="Jean Forteroche">
+        <title>Blog de Jean Forteroche - Billet simple pour l'Alaska</title>
+        <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="icon" href="/assets/img/favicon.ico">
+    </head>
+    <body>
+        <!--- NAVIGATION --->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <a href="/blog" class="navbar-brand">
+                <img src="/assets/img/logo.svg" width="280" height="50" alt="logo blog">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-expanded="false" aria-controls="navbar">
+                <span class="navbar-toggler-icon"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/blog">Retourner au site <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/admin/post">Poster un article </a>
+                    </li>
+                </ul>
+                </div>
+        </nav>
