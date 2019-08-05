33a7f5d0ac843543466e24fbcd84528d8107f289
Bosco91 <olivier@chanteloube.me>
2019-08-05 16:07:35 +0200

    Mise en place de la page about

--- views/blog/about.php
+++ views/blog/about.php
@@ -1,2 +1,14 @@
 <?php require VIEW_PATH . '/template/header.php'; ?>
-<h1>About</h1>
+
+    <div class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="about-intro text-uppercase">A propos de moi</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+<?php require VIEW_PATH . '/template/footer.php'; ?>
