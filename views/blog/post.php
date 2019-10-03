3101e25ab771c21e5d7126ea03f6fda20a6de4b1
Bosco91 <olivier@chanteloube.me>
2019-10-03 13:58:28 +0200

    Ajout de la vue des commentaires

--- views/blog/post.php
+++ views/blog/post.php
@@ -17,6 +17,22 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12 mx-auto">
+            <div class="comment-wrapper">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h4>Ajouter un Commentaire</h4>
+                    </div>
+                    <div class="panel-body">
+                        <textarea class="form-control" placeholder="Votre commentaire..." rows="3"></textarea>
+                        <br>
+                        <button type="button" class="btn btn-primary">Poster votre commentaire</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require VIEW_PATH . '/template/footer.php'; ?>
