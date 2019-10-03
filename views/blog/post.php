760ba463b008ec997e81936feb7af532259c6afa
Bosco91 <olivier@chanteloube.me>
2019-10-03 11:19:08 +0200

    stylisation page post

--- views/blog/post.php
+++ views/blog/post.php
@@ -1,17 +1,22 @@
 <?php require VIEW_PATH . '/template/header.php'; ?>
 <div class="containter">
     <div class="row">
-        <div class="title-post">
-            <h1><?php echo $singlepost['title']; ?></h1>
-            <p><?php echo $singlepost['date_post_fr']; ?></p>
-            <p><?php echo $singlepost['content']; ?></p>
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+                <h1><?php echo $singlepost['title']; ?></h1>
+                <div class="date-post">
+                    <p>Posté le <?php echo $singlepost['date_post_fr']; ?></p>
+                </div>
+            </div>
         </div>
     </div>
-
-
-
-
-
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-content">
+                <?php echo $singlepost['content']; ?>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php require VIEW_PATH . '/template/footer.php'; ?>
